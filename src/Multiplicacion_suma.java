8f2bb55fdd6db7070cef2c83ec93f7eb0fc96150
Franco adrian <adriantonanes@gmail.com>
2020-11-25 01:44:10 -0300

    Mejora del bucle

--- src/Multiplicacion_suma.java
+++ src/Multiplicacion_suma.java
@@ -1,14 +1,14 @@
 public class Multiplicacion_suma {
     static int suma_sucesiva(int x, int y){
         int suma = 0;
-        for(int i=1; i < y;i++){
-            suma = suma + x + x;
+        for(int i=1; i <= y;i++){
+            suma = suma + x;
         }
         return suma;
     }
     public static void main(String[] args) {
-        int x = 5;
-        int y = 2;
+        int x = 10;
+        int y = 5;
         int resultado = suma_sucesiva(x, y);
         System.out.println(resultado);
     }
